3aad90fe01dc9f9ea528afd0009f4db840424e77
inesf3 <ines.faivre@univ-lyon2.fr>
2024-02-19 14:05:12 +0100

    feat: add api similarity

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 from similarity_model import Model
+from fastapi import FastAPI
+import time
 
+
+app = FastAPI()
 model = Model()
-print(model.predict("I feel great", "I feel awesome"))
-#0.835
+
+@app.get("/")
+def home():
+    return {"Hello":"World"}
+
+@app.get("/similarity")
+def similarity(sentence1: str, sentence2: str):
+    start = time.time()
+    similar = model.predict(sentence1, sentence2)
+    end = time.time()
+    return {
+        "sentence1": sentence1,
+        "sentence2": sentence2,
+        "similarity": similar,
+        "time_taken": end - start
+        }
